9f886cb6c23d496db6a2194ec9281171725afd21
liangchurong <851903112@qq.com>
2026-07-26 23:36:16 +0800

    fix: faster fullscreen retry (50ms) to reduce flash

--- app/src/modules/ppt/PptCanvas.tsx
+++ app/src/modules/ppt/PptCanvas.tsx
@@ -43,13 +43,14 @@ export function PptCanvas({ data, onDataChange }: PptCanvasProps) {
             w: Math.round(w), h: Math.round(h), opacity: 1,
             props: { src, origW: ow, origH: oh, imgScale: w / ow },
           })
-          // Re-enter fullscreen if previewing (focus lost during paste)
+          // Re-enter fullscreen quickly if previewing
           if (st._previewing) {
             const retry = (n: number) => {
               if (n <= 0) return
-              getCurrentWindow().setFullscreen(true).catch(() => setTimeout(() => retry(n - 1), 200))
+              getCurrentWindow().setFullscreen(true).catch(() => {})
+              setTimeout(() => retry(n - 1), 50)
             }
-            setTimeout(() => retry(3), 150)
+            setTimeout(() => retry(2), 50)
           }
         }
       }
